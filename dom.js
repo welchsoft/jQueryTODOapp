e75c07078a0f4e3a5fc2eaa7fda4ba80fdf9aa9e
wade welch <wade.c.welch@gmail.com>
2018-05-30 09:45:43 -0500

    cleaned up syntax and added comments in  dom.js

--- dom.js
+++ dom.js
@@ -6,20 +6,22 @@ let completedTasks = $("#completed").sortable()
 
 $(document).ready(function(){
 
+
+//needed for click handler
   let taskAddBtn = $('#task-add-btn')
 
+//upon submission generates an <li>
+//the checkbox, textfield content, and remove button are children of the <li>
+//appends the populated <li> to the <ul>
   taskAddBtn.click(function(){
     let taskElement = $("<li>")
     let taskInput = $("<h3>")
-
-    let elementbtn = $("<button>")
-    elementbtn.html("remove")
+    let elementbtn = $("<button>").html("Remove")
     elementbtn.click(function(){
       $(this).parent().remove()
     })
-
+//sets up the checkbox which moves items between to do and done lists
     let elementCheckBox = $("<input>").attr("type","checkbox")
-
     elementCheckBox.change(function(){
         if(this.checked) {
           completedTasks.append($(this).parent())
@@ -28,16 +30,14 @@ $(document).ready(function(){
           pendingTasks.append($(this).parent())
         }
     })
-
+//append the child elements to <li> appends <li>
     taskElement.append(elementCheckBox)
     taskInput.append($("#task-input").val())
     taskElement.append(taskInput)
     taskElement.append(elementbtn)
     $("#task-input").val('')
-
+//append our completed task <li> to the <ul>
     pendingTasks.append(taskElement)
   })
-
-  let taskRemoveBtn = $('#task-remove-btn')
 
 })
